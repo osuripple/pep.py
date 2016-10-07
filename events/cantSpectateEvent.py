99f76e3227a2c90874929da703b108f47b14844f
Nyo <to@nyo.zz.mu>
2016-10-07 13:43:47 +0200

    .BANCHO. .FIX. Fix None token in cantSpectateEvent

--- events/cantSpectateEvent.py
+++ events/cantSpectateEvent.py
@@ -5,16 +5,13 @@ from objects import glob
 
 
 def handle(userToken, _):
-	# get usertoken data
-	userID = userToken.userID
-
 	try:
 		# We don't have the beatmap, we can't spectate
-		target = userToken.spectating
-		targetToken = glob.tokens.getTokenFromUserID(target)
+		if userToken.spectating not in glob.tokens.tokens:
+			raise exceptions.tokenNotFoundException
 
 		# Send the packet to host
-		targetToken.enqueue(serverPackets.noSongSpectator(userID))
+		glob.tokens.tokens[userToken.spectating].enqueue(serverPackets.noSongSpectator(userToken.userID))
 	except exceptions.tokenNotFoundException:
 		# Stop spectating if token not found
 		log.warning("Spectator can't spectate: token not found")
