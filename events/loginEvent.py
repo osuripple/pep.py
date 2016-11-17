b6e2319e8cd7bd5cee14b4175f6fb06c9eb69af4
Nyo <to@nyo.zz.mu>
2016-11-17 19:15:14 +0100

    .HIDE. Remove random prints, fix #9

--- events/loginEvent.py
+++ events/loginEvent.py
@@ -46,7 +46,6 @@ def handle(tornadoRequest):
 		splitData = loginData[2].split("|")
 		osuVersion = splitData[0]
 		timeOffset = int(splitData[1])
-		print(str(timeOffset))
 		clientData = splitData[3].split(":")[:5]
 		if len(clientData) < 4:
 			raise exceptions.forceUpdateException()
