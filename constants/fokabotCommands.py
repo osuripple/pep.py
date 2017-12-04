e3f1bc05e9bd0eb4f5a6982c145ae6325c22f57f
Giuseppe Guerra <to@nyo.zz.mu>
2017-12-04 23:34:04 +0100

    Changed !rtx privileges to ADMIN_KICK_USERS

--- constants/fokabotCommands.py
+++ constants/fokabotCommands.py
@@ -1272,7 +1272,7 @@ commands = [
 		"callback": switchServer
 	}, {
 		"trigger": "!rtx",
-		"privileges": privileges.ADMIN_MANAGE_SERVERS,
+		"privileges": privileges.ADMIN_KICK_USERS,
 		"syntax": "<username> <message>",
 		"callback": rtx
 	}
